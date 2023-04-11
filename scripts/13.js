09079333edda5afebfefefe5cca55168f1adb94f
WonilKim <nonelove2@gmail.com>
2023-04-11 09:28:41 +0900

    230410 Form Javascript

---
+++ scripts/13.js
@@ -0,0 +1,13 @@
+function show() {
+    //alert("안녕하세요.");
+
+    document.getElementById("h2Id").innerHTML = "안녕하세요";
+    // pico 와 충돌해서 잠깐 나왔다가 다시 사라짐.
+
+}
+
+// addEventListener : 이벤트 리스너
+// DOMContentLoaded : 컨텐츠가 다 로드되었을때.
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("h2Id").innerHTML = "DOMContentLoaded";
+});
